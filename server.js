4c55c4e24a8292fd31030029a91e0c5a91ff52c1
Amoasi474 <amoasikelvin3@gmail.com>
2026-03-12 03:15:00 +0000

    success page fixing

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const crypto = require("crypto");
 const { Pool } = require("pg");
 
 const app = express();
+app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
 const pool = new Pool({
